74876434ca04fd0470b0dce9a3e76bab9281bafe
leslierd23 <leslie.lrodri@gmail.com>
2023-10-25 18:35:23 -0500

    week6Project

---
+++ scripts/finalproject.js
@@ -0,0 +1,22 @@
+// Add click event to button "rbutton"
+document.querySelector('#rbutton').addEventListener('click', randomAnswer)
+document.querySelector('#rbutton').textContent = "Random Answer"
+
+//main function with fetch
+async function randomAnswer(){
+    console.log("Hola")
+    const randomOpt = Math.floor(Math.random()* 5)
+    const answer =await getRanAnsw(randomOpt)
+    document.querySelector('#ranswer').value= answer
+
+}
+ 
+
+async function getRanAnsw(randomOpt){
+
+    let apiResponse = await fetch('https://api.adviceslip.com/advice/search/love')
+    let apData = await apiResponse.json()
+    console.log("Random Answers", apData) 
+    return apData.slips[randomOpt].advice
+}
+
---
+++ scripts/getRan-answers.js
@@ -0,0 +1,7 @@
+export async function getRanAnsw(randomOpt){
+    let answerRandom = await fetch ('https://api.adviceslip.com/advice/search/love',
+    {cache:"default"})
+    .then(function(response){
+        let raData = await response.json()
+    }
+}
