2d5c3ff62c5df740fdb98729d20aed548cbe40ec
leslierd23 <leslie.lrodri@gmail.com>
2023-10-26 00:45:35 -0500

    w6

--- finalProject/getRan-answers.js
+++ finalProject/getRan-answers.js
@@ -2,6 +2,6 @@ export async function getRanAnsw(randomOpt){
     let answerRandom = await fetch ('https://api.adviceslip.com/advice/search/love',
     {cache:"default"})
     .then(function(response){
-        let raData = await response.json()
+        let raData = await.response.json()
     })
 }
